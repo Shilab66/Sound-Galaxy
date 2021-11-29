5075dbee990bdfef4fa39ca589efd4176d90ce8b
Aanshi6 <89350258+Shilab66@users.noreply.github.com>
2021-11-29 02:22:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+//let fft
+
+let Particle = function (position) {
+    this.position = position
+    this.speed = createVector(0, 1)
+    this.color = [random(0, 255), random(0, 255), random(0, 255)]
+
+    this.draw = function () {
+        circle(this.position.x, this.position.y, this.diameter)
+        fill(this.color)
+    }
+
+    this.update = function (energy) {
+        this.diameter = random(5, 7) + energy * 40
+        this.position.y += this.speed.y * energy * 20
+        if (this.position.y > height) {
+            this.position.y = 0
+        }
+    }
+}
+
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    noStroke();
+
+    let mic = new p5.AudioIn()
+    mic.start()
+
+    fft = new p5.FFT()
+    fft.setInput(mic)
+
+    positionParticles()
+}
+
+function draw(){
+    background(0, 0, 0)
+    let spectrum = fft.analyze()
+    updateParticles(spectrum)
+}
